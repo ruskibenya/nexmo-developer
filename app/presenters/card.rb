570875126a8fe2ab879bb191940a2c97a90c999f
Fabian Rodriguez <fabian.rodriguez@vonage.com>
2020-02-26 10:41:20 +0100

    [DEVX-2240] Add colour to use-cases and tutorials cards.

---
+++ app/presenters/card.rb
@@ -0,0 +1,20 @@
+class Card
+  COLOUR = {
+    'Voice' => 'green',
+    'SMS' => 'purple',
+    'Dispatch' => 'teal',
+    'Messages' => 'indigo',
+    'Client SDK' => 'orange',
+    'Conversation' => 'red',
+    'Number Insight' => 'grey',
+    'Verify' => 'aqua',
+    'voice' => 'green',
+    'messaging/sms' => 'purple',
+    'dispatch' => 'teal',
+    'messages' => 'indigo',
+    'client-sdk' => 'orange',
+    'conversation' => 'red',
+    'number-insight' => 'grey',
+    'verify' => 'aqua',
+  }.freeze
+end
